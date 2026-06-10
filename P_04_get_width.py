299c4b10494342a41b23f316c3d1f7775423bb72
khatrir50094-lgtm <khatrir50094@masseyhigh.school.nz>
2026-06-10 13:31:18 +1200

    Basic Width Checker

---
+++ P_04_get_width.py
@@ -0,0 +1,14 @@
+error= "Please enter a number that is more than zero\n"
+while True:
+
+    try:
+        width = float(input("Width: "))
+
+        if width > 0:
+            break
+        else:
+            print("error")
+
+    except ValueError:
+        print(error)
+
